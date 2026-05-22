74e47b46a8d352baf01da2e7f45fd8745ac4ded9
luoe <luoe@itu.dk>
2026-05-22 15:18:49 +0200

    added serial and uncommented motor tests and pump

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,39 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
 #include "userInput.h"
 #include "belt.h"
 #include "pump.h"
+#include "motors_test.h"
 
 extern int selectedDrink;   // from userInput.cpp
 extern bool startPressed;   // from userInput.cpp
 
 static bool machineRunning = false;
 
+
 void setup() {
+  /*motorsInit();*/
   userInputInit();
   beltInit();
   pumpInit();
+  Serial.begin(9600);
+
 }
 
 void loop() {
+  /*motorsUpdate();*/
+  
   userInputUpdate();
 
   // Start the machine when green button pressed and drink selected
   if (startPressed && !machineRunning && beltIdle()) {
+    Serial.println("A drink has been selected");
     machineRunning = true;
     beltStart(selectedDrink);
   }
 
   beltUpdate();
-  pumpUpdate();
+  //pumpUpdate();
 
   // Reset when belt returns home
   if (machineRunning && beltIdle()) {
